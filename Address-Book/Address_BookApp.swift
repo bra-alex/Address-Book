09a050bce7767e211792973c45af523a41b453c2
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-12-17 22:38:43 +0000

    Initial Commit

---
+++ Address-Book/Address_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Address_BookApp.swift
+//  Address-Book
+//
+//  Created by Don Bouncy on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Address_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Address-Book/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Address-Book
+//
+//  Created by Don Bouncy on 17/12/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
